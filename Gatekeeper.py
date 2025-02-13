3b486b0387585176dba0053ea7c93cfa890097f3
Pieter Naucke <554603@student.fontys.nl>
2025-02-13 10:58:32 +0100

    Versie 3

--- Gatekeeper.py
+++ Gatekeeper.py
@@ -1,9 +1,19 @@
 import datetime
 now = datetime.datetime.now()
 
-if now.hour>=7 and now.hour<=12:
-  print("Goedemorgen! Welkom bij Fonteyn Vakantieparken")
-elif now.hour>=12 and now.hour<=18:
-  print("Goedemiddag! Welkom bij Fonteyn Vakantieparken")
-elif now.hour>=18 and now.hour<=23:
-  print("Goedeavond! Welkom bij Fonteyn Vakantieparken")
+numberPlate = ["P11-09", "J12-32", "J62-59"] 
+
+print('Voer hier uw nummerplaat in:')
+x = input()
+
+if x in numberPlate:
+
+    if now.hour>=7 and now.hour<=12:
+        print("Goedemorgen! Welkom bij Fonteyn Vakantieparken")
+    elif now.hour>=12 and now.hour<=18:
+        print("Goedemiddag! Welkom bij Fonteyn Vakantieparken")
+    elif now.hour>=18 and now.hour<=23:
+        print("Goedeavond! Welkom bij Fonteyn Vakantieparken")
+
+else:
+    print ("verkeerde kenteken")
