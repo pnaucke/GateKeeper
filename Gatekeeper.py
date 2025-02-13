2b8d3d953455227314694887ace9b4f86ba0dee7
Pieter Naucke <554603@student.fontys.nl>
2025-02-13 10:49:02 +0100

    Versie 2

--- Gatekeeper.py
+++ Gatekeeper.py
@@ -1,1 +1,9 @@
-print("Welkom bij Fonteyn Vakantieparken")
+import datetime
+now = datetime.datetime.now()
+
+if now.hour>=7 and now.hour<=12:
+  print("Goedemorgen! Welkom bij Fonteyn Vakantieparken")
+elif now.hour>=12 and now.hour<=18:
+  print("Goedemiddag! Welkom bij Fonteyn Vakantieparken")
+elif now.hour>=18 and now.hour<=23:
+  print("Goedeavond! Welkom bij Fonteyn Vakantieparken")
